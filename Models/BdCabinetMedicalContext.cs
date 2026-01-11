98678db2d8e9c8b971f541b871508319f2611b19
BilalElAkry404 <Bilal.Elakry@emsi-edu.ma>
2026-01-11 18:32:04 +0100

    Final commit

--- Models/BdCabinetMedicalContext.cs
+++ Models/BdCabinetMedicalContext.cs
@@ -41,7 +41,7 @@ public partial class BdCabinetMedicalContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-TOR09GM;Database=BD_CabinetMedical;Trusted_Connection=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-LG6MRRR;Database=BD_CabinetMedical;Trusted_Connection=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -161,7 +161,7 @@ public partial class BdCabinetMedicalContext : DbContext
             entity.Property(e => e.Id).ValueGeneratedNever();
             entity.Property(e => e.NumSecuriteSociale).HasMaxLength(50);
 
-            
+
             entity.Property(e => e.DateNaissance)
                 .HasColumnType("date");
 
